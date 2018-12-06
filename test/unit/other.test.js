d2c8ef9f79e23424ac49d2b7c3ec6b0c36211bd2
juneandgreen <june_01@qq.com>
2018-12-07 01:39:09 +0800

    fixed test case

--- test/unit/other.test.js
+++ test/unit/other.test.js
@@ -3,13 +3,13 @@ const simulate = require('../../index')
 test('sleep', async () => {
     let start = +new Date()
     await simulate.sleep(50)
-    expect(+new Date - start >= 50).toBe(true)
+    expect(+new Date() - start >= 50).toBe(true)
 
     start = +new Date()
     await simulate.sleep(100)
-    expect(+new Date - start >= 100).toBe(true)
+    expect(+new Date() - start >= 100).toBe(true)
 
     start = +new Date()
     await simulate.sleep()
-    expect(+new Date - start >= 0).toBe(true)
+    expect(+new Date() - start >= 0).toBe(true)
 })
