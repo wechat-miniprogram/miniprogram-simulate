e36a17dbcafa16400e85743e3100aa44f5da5650
juneandgreen <june_01@qq.com>
2019-09-26 12:05:14 +0800

    update: add demo for render property

---
+++ test/comp6/index.js
@@ -0,0 +1,20 @@
+Component({
+    properties: {
+        hasChild: {
+            type: Boolean,
+            value: false,
+        },
+        prop: {
+            type: String,
+            value: 'index.properties',
+            observer(newVal, oldVal) {
+                this.setData({
+                    observerArr: [newVal, oldVal],
+                })
+            }
+        },
+    },
+    data: {
+        observerArr: [],
+    },
+})
---
+++ test/comp6/index.test.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const simulate = require('../../index')
+
+function runTest(id) {
+    const comp = simulate.render(id, {prop: 'index.test.properties', hasChild: true})
+
+    const parent = document.createElement('parent-wrapper')
+    comp.attach(parent)
+
+    console.log(comp.dom.innerHTML)
+    expect(simulate.match(comp.dom, `
+        <wx-view class="main--index">index.test.properties</wx-view>
+        <wx-view>index.test.properties</wx-view>
+        <wx-view>index.properties</wx-view>
+    `)).toBe(true)
+    expect(window.getComputedStyle(comp.querySelector('.index').dom).color).toBe('green')
+    expect(comp.dom.tagName).toBe('MAIN')
+
+    expect(comp.instance.data.observerArr).toEqual(['index.test.properties', 'index.properties'])
+}
+
+test('comp6', () => {
+    let id = simulate.load(path.resolve(__dirname, './index'))
+    runTest(id)
+
+    jest.resetModules() // https://github.com/facebook/jest/issues/5120
+
+    id = simulate.load(path.resolve(__dirname, './index'), {compiler: simulate})
+    runTest(id)
+})
