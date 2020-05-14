409f5d1b0404cdaaf610c84d3e2eab4e5ef501c6
Tidyzq <tidyzq@tidyzq.com>
2020-05-14 20:16:00 +0800

    type: add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+import { ComponentId, Component } from 'j-component'
+
+export {
+    behavior,
+    Component,
+    ComponentJSON,
+    ComponentId,
+    RootComponent,
+    create as render
+} from 'j-component'
+
+export interface LoadOptions {
+    compiler?: 'official' | 'simulate';
+    rootPath?: string;
+    less?: boolean;
+}
+
+export function load<
+    TData extends WechatMiniprogram.Component.DataOption,
+    TProperty extends WechatMiniprogram.Component.PropertyOption,
+    TMethod extends WechatMiniprogram.Component.MethodOption
+>(
+    options: WechatMiniprogram.Component.Options<TData, TProperty, TMethod> & {
+        id?: string;
+        tagName?: string;
+        template?: string;
+        usingComponents?: Object;
+    },
+): ComponentId<TData, TProperty, TMethod>
+export function load(componentPath: string, options?: LoadOptions): string
+export function load(componentPath: string, tagName: string, options?: LoadOptions): string
+
+export function match(dom: Node, html: string): boolean;
+
+export function sleep(timeout: number): Promise<void>;
+
+export function scroll(component: Component<any, any, any>, destOffset: number, times?: number, propName?: string): void;
