a9ea984db2af17724145d58646e4ddc847afd508
Tidyzq <tidyzq@tidyzq.com>
2020-05-18 15:28:16 +0800

    doc: add comments for jest-snapshot-plugin

--- src/jest-snapshot-plugin.js
+++ src/jest-snapshot-plugin.js
@@ -1,14 +1,49 @@
+/**
+ * 参考实现 https://github.com/facebook/jest/blob/3f107a3377/packages/pretty-format/src/plugins/ReactTestComponent.ts
+ */
 const _markup = require('pretty-format/build/plugins/lib/markup')
 
+/**
+ * 用于识别 j-component toJSON 函数生成的 JSON 对象
+ */
 const testSymbol =
   typeof Symbol === 'function' && Symbol.for
       ? Symbol.for('j-component.json')
       : 0xd846fe
 
+/**
+ * @typedef {object} JComponentJSON j-component toJSON 函数生成的 JSON 对象
+ * @prop {string} tagName
+ * @prop {{ name: string; value: any }[]} attrs
+ * @prop {{ [event: string]: { handler: string; isCapture: boolean; isMutated: boolean; isCatch: boolean; name: string } }} event
+ * @prop {JComponentJSON[]} children
+ */
+
+/**
+ * @typedef {import('pretty-format/build/types').NewPlugin} NewPlugin
+ * @typedef {import('pretty-format/build/types').Config} Config
+ * @typedef {import('pretty-format/build/types').Printer} Printer
+ * @typedef {import('pretty-format/build/types').Refs} Refs
+ */
+
+/**
+ * 暴露给 pretty-format 的检测函数
+ * 仅有检测返回 true 的对象才会传入 serialize
+ * @param {any} val
+ */
 const test = function test(val) {
     return val && val.$$typeof === testSymbol
 }
 
+/**
+ * 打印节点属性
+ * @param {JComponentJSON['attrs']} attrs
+ * @param {Config} config
+ * @param {string} indentation
+ * @param {number} depth
+ * @param {Refs} refs
+ * @param {Printer} printer
+ */
 const printAttrs = function printAttrs(
     attrs,
     config,
@@ -31,11 +66,11 @@ const printAttrs = function printAttrs(
             if (typeof value !== 'string') {
                 if (printed.indexOf('\n') !== -1) {
                     printed =
-            config.spacingOuter +
-            indentationNext +
-            printed +
-            config.spacingOuter +
-            indentation
+                        config.spacingOuter +
+                        indentationNext +
+                        printed +
+                        config.spacingOuter +
+                        indentation
                 }
 
                 printed = '"{{' + printed + '}}"'
@@ -43,19 +78,28 @@ const printAttrs = function printAttrs(
 
             return (
                 config.spacingInner +
-        indentation +
-        colors.prop.open +
-        name +
-        colors.prop.close +
-        '=' +
-        colors.value.open +
-        printed +
-        colors.value.close
+                indentation +
+                colors.prop.open +
+                name +
+                colors.prop.close +
+                '=' +
+                colors.value.open +
+                printed +
+                colors.value.close
             )
         })
         .join('')
 }
 
+/**
+ * 打印节点事件绑定
+ * @param {JComponentJSON['event']} event
+ * @param {Config} config
+ * @param {string} indentation
+ * @param {number} depth
+ * @param {Refs} refs
+ * @param {Printer} printer
+ */
 const printEvent = function printEvent(
     event,
     config,
@@ -75,28 +119,37 @@ const printEvent = function printEvent(
             const isMutated = eventInfo.isMutated
             const isCatch = eventInfo.isCatch
             const attrName =
-        (isCapture ? 'capture-' : '') +
-        (isMutated ? 'mut-' : '') +
-        (isCatch ? 'catch' : 'bind') +
-        ':' +
-        eventName
+                (isCapture ? 'capture-' : '') +
+                (isMutated ? 'mut-' : '') +
+                (isCatch ? 'catch' : 'bind') +
+                ':' +
+                eventName
 
             const printed = printer(handler, config, indentationNext, depth, refs)
             return (
                 config.spacingInner +
-        indentation +
-        colors.prop.open +
-        attrName +
-        colors.prop.close +
-        '=' +
-        colors.value.open +
-        printed +
-        colors.value.close
+                indentation +
+                colors.prop.open +
+                attrName +
+                colors.prop.close +
+                '=' +
+                colors.value.open +
+                printed +
+                colors.value.close
             )
         })
         .join('')
 }
 
+/**
+ * 打印节点所有属性
+ * @param {JComponentJSON} object
+ * @param {Config} config
+ * @param {string} indentation
+ * @param {number} depth
+ * @param {Refs} refs
+ * @param {Printer} printer
+ */
 const printProps = function printProps(
     object,
     config,
@@ -107,10 +160,20 @@ const printProps = function printProps(
 ) {
     return (
         printAttrs(object.attrs, config, indentation, depth, refs, printer) +
-    printEvent(object.event, config, indentation, depth, refs, printer)
+        printEvent(object.event, config, indentation, depth, refs, printer)
     )
 }
 
+/**
+ * 暴露给 pretty-format 的打印函数
+ * 仅有通过 test 函数检测返回 true 的对象才会传入 serialize
+ * @param {JComponentJSON} object
+ * @param {Config} config
+ * @param {string} indentation
+ * @param {number} depth
+ * @param {Refs} refs
+ * @param {Printer} printer
+ */
 const serialize = function serialize(
     object,
     config,
@@ -144,9 +207,7 @@ const serialize = function serialize(
         )
 }
 
-const plugin = {
+module.exports = {
     serialize,
     test,
 }
-
-module.exports = plugin
