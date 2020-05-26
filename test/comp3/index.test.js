198449bdf180141c95674a66416235075d70cfb0
junexie <junexie@tencent.com>
2020-05-26 20:03:45 +0800

    update: test/comp3

--- test/comp3/index.test.js
+++ test/comp3/index.test.js
@@ -13,7 +13,7 @@ async function runTest(id) {
 
     view.dispatchEvent('touchstart')
     view.dispatchEvent('touchend')
-    await simulate.sleep(500)
+    await simulate.sleep(200)
     expect(view.dom.innerHTML).toBe('<div>comp3.properties</div>')
 
     expect(comp.instance.print()).toBe(123)
